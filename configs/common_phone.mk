754abbe045ae825b6e6af7d535a08abc5554eba7
Chris41g <chriswhite@cableone.net>
2012-06-20 00:36:01 -0500

    add su

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -1,6 +1,5 @@
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
 -PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk     
-    
-    
+    vendor/aokp/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk \    
+    vendor/aokp/prebuilt/common/app/su:system/xbin/su

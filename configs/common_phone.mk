a3b0c2be424db8e47ecbf7b9fbac15381bba7bad
Chris41g <chriswhite@cableone.net>
2012-06-19 21:30:59 -0500

    Add SuperSU

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -1,4 +1,6 @@
 $(call inherit-product, vendor/aokp/configs/common.mk)
-     
+
+-PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk     
     
     

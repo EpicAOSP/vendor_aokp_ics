84746eb9ffed3f2486c2ef77c8349b1f7cbe28a0
Chris41g <chriswhite@cableone.net>
2012-06-30 18:55:50 -0500

    Add novalauncher back

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -1,9 +1,10 @@
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk \
+	vendor/aokp/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk \
 	vendor/aokp/prebuilt/common/app/su:system/xbin/su \
-		device/samsung/epic4gtouch/configs/apns-conf.xml:system/etc/apns-conf.xml
+	vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk \
+	device/samsung/epic4gtouch/configs/apns-conf.xml:system/etc/apns-conf.xml
 
 
 # Inherit drm blobs
